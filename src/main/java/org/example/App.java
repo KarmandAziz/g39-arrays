4bd89cf2751ac9641d4bfae1d5b8365059f1ccf1
KarmandAziz <Karmand94@gmail.com>
2021-10-14 23:38:08 +0200

    Lecture

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,32 +1,47 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
-/**
- * Arrays
- *
- */
 public class App 
 {
+
     public static void main( String[] args) {
 
+       int [][] numbers = new int [5][5];
+       numbers [0][0] = 1;
+         for (int[] array : numbers) {
+             int index = 0;
+             for(int number : array) {
+                 System.out.println(number);
+                 if(index ==array.length -1) {
+                     System.out.println();
+                 }
+                 index++;
+             }
+         }
+
+
+
+
+    }
+
+    public static void arrayConcatExample(){
         String[] source = {"Foo", "Bar", "Baz"};
         String[] toAdd = {"Boo", "Baa"};
 
         String[] result = addToStringArray(source, toAdd);
         System.out.println(arrayToString(result));
-
-
     }
 
     public static String[] addToStringArray(final String[] source, String...strings) {
-        String[] newArray = new String[source.length + strings.length];
-        for (int i = 0; i < source.length; i++) {
-            newArray[i] = source[i];
-        }
+        String[] newArray = Arrays.copyOf(source, source.length + strings.length);
         for (int iWrite = source.length, iRead = 0; iRead<strings.length; iWrite++, iRead++){
             newArray[iWrite] = strings[iRead];
         }
+
         return newArray;
     }
 
@@ -49,7 +64,7 @@ public class App
         while(running){
             System.out.println("Write a task: ");
             String task = scanner.nextLine();
-            todos = addToStringArray(todos, task);
+            todos = addToStringArray(to dos, task);
             System.out.println(arrayToString(todos));
             System.out.println("Do you want to continue? (y / n)");
             switch (scanner.nextLine()){
@@ -61,21 +76,19 @@ public class App
     }
 
     public static String[] addToStringArray(final String[] source, final String string) {
-        String[] newArray = new String[source.length + 1];
-        for(int i=0; i<source.length; i++){
-            newArray[i] = source[i];
-        }
+        String[] newArray = Arrays.copyOf(source, source.length + 1);
         newArray[newArray.length -1] = string;
-        return newArray;
+
+            return newArray;
     }
 
     public static String findTask(String task, String[] array) {
-        for(String string: array) {
-            if(string.equalsIgnoreCase(task)){
-                return string;
-            }
-        }
-        return null;
+        Arrays.sort(array);
+        int index = Arrays.binarySearch(array, task);
+        if (index < 0){
+            return null;
+    }
+       return array[index];
     }
 
     public static String arrayToString(String[] strings) {
@@ -89,6 +102,16 @@ public class App
         message += "]";
         return message;
     }
+
+    public static String preferredFindTask(String task, String[] array) {
+        for(String string : array) {
+            if(task.equals(string)){
+                return task;
+            }
+        }
+        return null;
+    }
+
 }
 
 
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,13 +3,14 @@ package org.example;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     @Test
     public void arrayToStringReturnExpectedResult() {
         //Arrange - Setting up the test
@@ -23,6 +24,7 @@ public class AppTest
         assertEquals(actual, expected);
 
     }
+
     @Test
     public void testAddToStringArray() {
         String[] testArrayData = {};
@@ -33,4 +35,16 @@ public class AppTest
 
         assertArrayEquals(actual, expected);
     }
+    @Test
+    public void findTask(){
+        String[] data = {"Basel", "Karmand", "Erik", "Fabrice"};
+        String task = "Fabrice";
+        String expected = "Fabrice";
+
+        String actual = App.findTask(task, data);
+        System.out.println(Arrays.toString(data));
+
+        assertEquals(expected, actual);
+    }
+
 }
